7d86cae808a344f429c4149cc38a463c49f42963
nguyenphandiemmy2004-lang <nguyenphandiemmy2004@gmail.com>
2026-07-27 14:05:13 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,3 +34,34 @@ function prevSlide() {
 /* Tự động chạy */
 setInterval(nextSlide, 3000);
 
+// ===============================
+// HIỂN THỊ SẢN PHẨM
+// ===============================
+
+const productList = document.getElementById("product-list");
+function displayProducts() {
+  if (!productList) return;
+  let html = "";
+  books.forEach((book) => {
+    html += `
+        <div class="product">
+            <img src="${book.image}" alt="${book.name}">
+            <h3>${book.name}</h3>
+            <p>${book.author}</p>
+            <p>${book.category}</p>
+            <p class="price">
+                ${book.price.toLocaleString()} đ
+            </p>
+            <button class="detail-btn">
+                Xem chi tiết
+            </button>
+            <button class="cart-btn">
+                Thêm vào giỏ
+            </button>
+        </div>
+        `;
+  });
+  productList.innerHTML = html;
+}
+displayProducts();
+
