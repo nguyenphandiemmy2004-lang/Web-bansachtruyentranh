2ecfe68323c304c2235406c71f3a75ea4620d6de
nguyenphandiemmy2004-lang <nguyenphandiemmy2004@gmail.com>
2026-07-27 13:53:16 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,3 +27,39 @@ function prevSlide() {
 /* Tự động chuyển ảnh sau 3 giây */
 setInterval(nextSlide, 3000);
 
+/*==========================
+        SLIDER
+===========================*/
+
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+/* Hiển thị banner */
+function showSlide(index) {
+  slides.forEach(function (slide) {
+    slide.classList.remove("active");
+  });
+  slides[index].classList.add("active");
+}
+
+/* Banner tiếp theo */
+function nextSlide() {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  showSlide(currentSlide);
+}
+
+/* Banner trước */
+function prevSlide() {
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
+  showSlide(currentSlide);
+}
+
+/* Tự động chạy */
+setInterval(nextSlide, 3000);
+
