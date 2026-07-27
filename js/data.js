0f4815276c61bab3b130efb497d6fb9c7a1d4d2a
nguyenphandiemmy2004-lang <nguyenphandiemmy2004@gmail.com>
2026-07-27 14:01:47 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,78 @@
+// ===============================
+// DANH SÁCH SÁCH
+// ===============================
+
+const books = [
+  {
+    id: 1,
+    name: "Đắc Nhân Tâm",
+    author: "Dale Carnegie",
+    category: "Kỹ năng",
+    price: 120000,
+    image: "images/book1.jpg",
+  },
+
+  {
+    id: 2,
+    name: "Nhà Giả Kim",
+    author: "Paulo Coelho",
+    category: "Tiểu thuyết",
+    price: 135000,
+    image: "images/book2.jpg",
+  },
+
+  {
+    id: 3,
+    name: "Harry Potter",
+    author: "J.K.Rowling",
+    category: "Fantasy",
+    price: 250000,
+    image: "images/book3.jpg",
+  },
+
+  {
+    id: 4,
+    name: "One Piece",
+    author: "Eiichiro Oda",
+    category: "Manga",
+    price: 95000,
+    image: "images/book4.jpg",
+  },
+
+  {
+    id: 5,
+    name: "Conan",
+    author: "Aoyama Gosho",
+    category: "Manga",
+    price: 40000,
+    image: "images/book5.jpg",
+  },
+
+  {
+    id: 6,
+    name: "Doraemon",
+    author: "Fujiko F. Fujio",
+    category: "Thiếu nhi",
+    price: 35000,
+    image: "images/book6.jpg",
+  },
+
+  {
+    id: 7,
+    name: "Lập Trình Web",
+    author: "Nguyễn Văn A",
+    category: "CNTT",
+    price: 180000,
+    image: "images/book7.jpg",
+  },
+
+  {
+    id: 8,
+    name: "Clean Code",
+    author: "Robert C. Martin",
+    category: "CNTT",
+    price: 320000,
+    image: "images/book8.jpg",
+  },
+];
 
