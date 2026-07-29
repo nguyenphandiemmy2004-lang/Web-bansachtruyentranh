54805d6033ea1bf5f47d2b12f2be7efbe8ef67ec
nguyenphandiemmy2004-lang <nguyenphandiemmy2004@gmail.com>
2026-07-29 15:06:54 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,80 +3,93 @@
 // ===============================
 
 const books = [
-  {
-  id: 1,
-  name: "Đắc Nhân Tâm",
-  author: "Dale Carnegie",
-  category: "Kỹ năng",
-  year: 1936,
-  price: 120000,
-  image: "images/book1.jpg",
-  description: "Cuốn sách nổi tiếng giúp phát triển kỹ năng giao tiếp, ứng xử và nghệ thuật đối nhân xử thế."
-  },
 
-  {
-  id:2,
-  name:"Nhà Giả Kim",
-  author:"Paulo Coelho",
-  category:"Tiểu thuyết",
-  year:1988,
-  price:135000,
-  image:"images/book2.jpg",
-  description:"Cuốn tiểu thuyết truyền cảm hứng về hành trình theo đuổi ước mơ."
-  },
+{
+    id:1,
+    name:"Đắc Nhân Tâm",
+    author:"Dale Carnegie",
+    category:"Kỹ năng",
+    year:1936,
+    description:"Cuốn sách nổi tiếng giúp phát triển kỹ năng giao tiếp, ứng xử và tạo ảnh hưởng tích cực trong cuộc sống.",
+    price:120000,
+    image:"images/book1.jpg"
+},
 
-  {
-    id: 3,
-    name: "Harry Potter",
-    author: "J.K.Rowling",
-    category: "Fantasy",
-    price: 250000,
-    image: "images/book3.jpg",
-  },
+{
+    id:2,
+    name:"Nhà Giả Kim",
+    author:"Paulo Coelho",
+    category:"Tiểu thuyết",
+    year:1988,
+    description:"Câu chuyện truyền cảm hứng về hành trình theo đuổi ước mơ và khám phá ý nghĩa cuộc sống.",
+    price:135000,
+    image:"images/book2.jpg"
+},
 
-  {
-    id: 4,
-    name: "One Piece",
-    author: "Eiichiro Oda",
-    category: "Manga",
-    price: 95000,
-    image: "images/book4.jpg",
-  },
+{
+    id:3,
+    name:"Harry Potter",
+    author:"J.K. Rowling",
+    category:"Fantasy",
+    year:1997,
+    description:"Tiểu thuyết giả tưởng nổi tiếng kể về cậu bé phù thủy Harry Potter cùng những cuộc phiêu lưu kỳ thú.",
+    price:250000,
+    image:"images/book3.jpg"
+},
 
-  {
-    id: 5,
-    name: "Conan",
-    author: "Aoyama Gosho",
-    category: "Manga",
-    price: 40000,
-    image: "images/book5.jpg",
-  },
+{
+    id:4,
+    name:"One Piece",
+    author:"Eiichiro Oda",
+    category:"Manga",
+    year:1997,
+    description:"Bộ truyện tranh nổi tiếng kể về hành trình của Monkey D. Luffy đi tìm kho báu One Piece.",
+    price:95000,
+    image:"images/book4.jpg"
+},
 
-  {
-    id: 6,
-    name: "Doraemon",
-    author: "Fujiko F. Fujio",
-    category: "Thiếu nhi",
-    price: 35000,
-    image: "images/book6.jpg",
-  },
+{
+    id:5,
+    name:"Conan",
+    author:"Aoyama Gosho",
+    category:"Manga",
+    year:1994,
+    description:"Bộ truyện trinh thám nổi tiếng xoay quanh thám tử nhí Conan Edogawa giải quyết các vụ án ly kỳ.",
+    price:40000,
+    image:"images/book5.jpg"
+},
 
-  {
-    id: 7,
-    name: "Lập Trình Web",
-    author: "Nguyễn Văn A",
-    category: "CNTT",
-    price: 180000,
-    image: "images/book7.jpg",
-  },
+{
+    id:6,
+    name:"Doraemon",
+    author:"Fujiko F. Fujio",
+    category:"Thiếu nhi",
+    year:1969,
+    description:"Bộ truyện tranh thiếu nhi kinh điển về chú mèo máy Doraemon và những bảo bối đến từ tương lai.",
+    price:35000,
+    image:"images/book6.jpg"
+},
 
-  {
-    id: 8,
-    name: "Clean Code",
-    author: "Robert C. Martin",
-    category: "CNTT",
-    price: 320000,
-    image: "images/book8.jpg",
-  },
+{
+    id:7,
+    name:"Lập Trình Web",
+    author:"Nguyễn Văn A",
+    category:"CNTT",
+    year:2023,
+    description:"Sách hướng dẫn học HTML, CSS và JavaScript từ cơ bản đến nâng cao dành cho sinh viên và người mới bắt đầu.",
+    price:180000,
+    image:"images/book7.jpg"
+},
+
+{
+    id:8,
+    name:"Clean Code",
+    author:"Robert C. Martin",
+    category:"CNTT",
+    year:2008,
+    description:"Cuốn sách kinh điển dành cho lập trình viên, hướng dẫn cách viết mã nguồn sạch, dễ đọc và dễ bảo trì.",
+    price:320000,
+    image:"images/book8.jpg"
+}
+
 ];
-
