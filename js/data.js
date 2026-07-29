f62d6725ce8c599b71f8ba6b17ad6ceaea0899f9
nguyenphandiemmy2004-lang <nguyenphandiemmy2004@gmail.com>
2026-07-29 17:05:39 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -12,7 +12,7 @@ const books = [
     year: 1936,
     publisher: "NXB Văn Học",
     description: "Cuốn sách nổi tiếng giúp phát triển kỹ năng giao tiếp, ứng xử và tạo ảnh hưởng tích cực trong cuộc sống.",
-    price: 120000,
+    price: 86000,
     image: "images/book1.jpg"
 },
 
@@ -22,75 +22,60 @@ const books = [
     author:"Paulo Coelho",
     category:"Tiểu thuyết",
     year:1988,
-    description:"Câu chuyện truyền cảm hứng về hành trình theo đuổi ước mơ và khám phá ý nghĩa cuộc sống.",
+    publisher: " ",
+    description:"Truyện xoay quanh cuộc phiêu lưu của cậu bé chăn cừu Santiago người Tây Ban Nha đi tìm kho báu ở kim tự tháp Ai Cập theo những giấc mơ lặp đi lặp lại. Trên hành trình băng qua sa mạc, cậu gặp gỡ nhiều người (ông vua xứ Salem, người buôn pha lê, Nhà Giả Kim...) và học cách lắng nghe trái tim, thấu hiểu những điềm báo của vũ trụ.",
     price:135000,
     image:"images/book2.jpg"
 },
 
 {
     id:3,
-    name:"Harry Potter",
-    author:"J.K. Rowling",
-    category:"Fantasy",
-    year:1997,
-    description:"Tiểu thuyết giả tưởng nổi tiếng kể về cậu bé phù thủy Harry Potter cùng những cuộc phiêu lưu kỳ thú.",
-    price:250000,
+    name:"Doraemon Truyện Dài - Vol.23: Nobita Và Những Pháp Sư Gió Bí Ẩn",
+    author:"Fujiko F. Fujio",
+    category:"Thiếu nhi",
+    year: Xuất bản lần đầu năm 2003 (tại Nhật),
+    publisher: "Nhà xuất bản Kim Đồng",
+    description:"cuộc phiêu lưu đầy kịch tính của nhóm bạn Doraemon tại Vương quốc Gió để bảo vệ cậu bé thần gió Fuuko và ngăn chặn âm mưu hồi sinh con quỷ rồng Mafuga.",
+    price:22000,
     image:"images/book3.jpg"
 },
 
 {
     id:4,
-    name:"One Piece",
-    author:"Eiichiro Oda",
-    category:"Manga",
-    year:1997,
-    description:"Bộ truyện tranh nổi tiếng kể về hành trình của Monkey D. Luffy đi tìm kho báu One Piece.",
-    price:95000,
+    name:"Sổ Tay "Dân IT" Lập Trình Web, Lập Trình Ứng Dụng Từ Cơ Bản Đến Chuyên Sâu",
+    author:"Đào Xuân Hiệp",
+    category:"Sách công nghệ thông tin",
+    year:2024,
+    publisher: "NXB Thanh Niên",
+    description:"Sách hướng dẫn học HTML, CSS và JavaScript từ cơ bản đến nâng cao dành cho sinh viên và người mới bắt đầu.",
+    price:199000,
     image:"images/book4.jpg"
 },
 
 {
     id:5,
-    name:"Conan",
-    author:"Aoyama Gosho",
-    category:"Manga",
-    year:1994,
-    description:"Bộ truyện trinh thám nổi tiếng xoay quanh thám tử nhí Conan Edogawa giải quyết các vụ án ly kỳ.",
-    price:40000,
-    image:"images/book5.jpg"
-},
-
-{
-    id:6,
-    name:"Doraemon",
-    author:"Fujiko F. Fujio",
-    category:"Thiếu nhi",
-    year:1969,
-    description:"Bộ truyện tranh thiếu nhi kinh điển về chú mèo máy Doraemon và những bảo bối đến từ tương lai.",
-    price:35000,
-    image:"images/book6.jpg"
-},
-
-{
-    id:7,
-    name:"Lập Trình Web",
-    author:"Nguyễn Văn A",
-    category:"CNTT",
-    year:2023,
-    description:"Sách hướng dẫn học HTML, CSS và JavaScript từ cơ bản đến nâng cao dành cho sinh viên và người mới bắt đầu.",
-    price:180000,
-    image:"images/book7.jpg"
-},
-
-{
-    id:8,
     name:"Clean Code",
     author:"Robert C. Martin",
-    category:"CNTT",
+    category:"Sách công nghệ thông tin",
     year:2008,
+    publisher: "Prentice Hall /Pearson Education.",
     description:"Cuốn sách kinh điển dành cho lập trình viên, hướng dẫn cách viết mã nguồn sạch, dễ đọc và dễ bảo trì.",
-    price:320000,
-    image:"images/book8.jpg"
+    price:294000,
+    image:"images/book5.jpg"
 }
+
+{
+    id: 6,
+    name: "Hàng Xóm Của Tôi Totoro",
+    author: "Từ bộ phim của Hayao Miyazaki",
+    category: "Manga",
+    year: 2026,
+    publisher: "Kim Đồng",
+    description: "Hàng Xóm Của Tôi Totoro là câu chuyện trong trẻo, ấm áp về chuyến hành trình khám phá thế giới kỳ diệu và tình bạn kỳ lạ giữa hai chị em nhỏ với vị thần rừng tốt bụng Totoro ở vùng quê thanh bình.",
+    price: 120000,
+    image: "images/book6.jpg"
+},
+
+
 
 ];
